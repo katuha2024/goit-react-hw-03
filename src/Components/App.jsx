11d95a115e4ccbad217cfca5a333192247940407
katuha2024 <ukrbrend2020@gmail.com>
2025-04-27 16:33:10 +0300

    finish task

---
+++ src/Components/App.jsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
+import ContactForm from '../Components/ContactForm/ContactForm';
+import ContactList from '../Components/ContactList/ContactList';
+import SearchBox from '../Components/SearchBox/SearchBox';
+import './App.css';
+
+function App() {
+  const initialPhoneBook = [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ];
+
+  const initAddContactValues = {
+    contactName: "",
+    contactPhone: "",
+  };
+
+  const [phoneBook, setPhoneBook] = useState(
+    JSON.parse(window.localStorage.getItem('userContacts')) ?? initialPhoneBook
+  );
+  const [searchPhrase, setSearchPhrase] = useState('');
+
+  useEffect(() => {
+    window.localStorage.setItem('userContacts', JSON.stringify(phoneBook));
+  }, [phoneBook]);
+
+  const handleSubmit = (values) => {
+    setPhoneBook([
+      ...phoneBook,
+      { id: nanoid(), name: values.contactName, number: values.contactPhone },
+    ]);
+  };
+
+  const clickDeleteButton = (id) => {
+    setPhoneBook((prevPhoneBook) =>
+      prevPhoneBook.filter((contact) => contact.id !== id)
+    );
+  };
+
+  const handleChange = (evt) => {
+    setSearchPhrase(evt.target.value);
+  };
+
+  const filteredContacts = phoneBook.filter((contact) =>
+    contact.name.toLowerCase().includes(searchPhrase.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <div className="Wrap">
+        <div className="componentsWrap">
+          <ContactForm onSubmit={handleSubmit} initValues={initAddContactValues} />
+          <SearchBox value={searchPhrase} onChange={handleChange} />
+        </div>
+        <div className="Wrap">
+          <ContactList contacts={filteredContacts} onDelete={clickDeleteButton} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/ContactList/ContactList.jsx
@@ -0,0 +1,21 @@
+import styles from './ContactList.module.css';
+import Contact from '../ContactList/Contact';
+
+export default function ContactList({ contacts, onDelete }) {
+  return (
+    <div className={styles.container}>
+      {contacts.length === 0
+        ? <p>Dear Mentor, no such contact found, try again! )))</p>
+        : (<ul className={styles.contactList}>
+          {contacts.map(contact => {
+            return (
+              <li key={contact.id} className={styles.listItem}>
+                <Contact people={contact} onClickDelete={onDelete} />
+              </li>
+            )
+          })}
+        </ul>)
+      }
+    </div>
+  );
+}
