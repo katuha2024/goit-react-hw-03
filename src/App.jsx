988ae91730f5dffba75d63784bbb177a07503343
katuha2024 <ukrbrend2020@gmail.com>
2025-04-30 07:50:20 +0300

    change components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
-import ContactForm from './components/contactform/ContactForm.jsx';
-import ContactList from './components/contactlist/ContactList.jsx';
-import SearchBox from './components/searchBox/SearchBox.jsx';
+import ContactForm from './components2/contactform/ContactForm.jsx';
+import ContactList from './components2/contactlist/ContactList.jsx';
+import SearchBox from './components2/searchbox/SearchBox.jsx';
 import './App.css';
 
 
@@ -13,6 +13,7 @@ function App() {
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     { id: 'id-5', name: 'Ira', number: '0683436265' },
+    {id: 'id-7', name: 'Katya', number: '0982916397'},
   ];
 
   const initAddContactValues = {
